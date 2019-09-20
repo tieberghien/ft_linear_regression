3279b54649f7bf1c629f35bd70979a3974d7bb04
Estelle TIEBERGHIEN <etieberg@e1r2p16.42.fr>
2019-09-20 15:13:38 +0200

    data plotted

--- main.py
+++ main.py
@@ -22,9 +22,43 @@ def calculateCost(x,theta,Y):
     prediction = predictPrice(x,theta)
     return ((prediction - Y)**2).mean()/2
 
+def plot(X, Y, b0, b1):
+#plotting values 
+    x_max = np.max(X) + 100
+    x_min = np.min(X) - 100
+    #calculating line values of x and y
+    x = np.linspace(x_min, x_max, 1000)
+    y = b0 + b1 * x
+    #plotting line 
+    plt.plot(x, y, color='#00ff00', label='Linear Regression')
+    #plot the data point
+    plt.scatter(X, Y, color='#ff0000', label='Data Point')
+    # x-axis label
+    plt.xlabel('Kilometers')
+    #y-axis label
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+
 def main():
-    data = pd.read_csv("data.csv") 
-    print(data['km'].values)
+    data = pd.read_csv("data.csv")
+    X = data['km'].values
+    Y = data['price'].values
+
+    x_mean = np.mean(X)
+    y_mean = np.mean(Y)
+
+    n = len(X)
+    numerator = 0
+    denominator = 0
+    for i in range(n):
+        numerator += (X[i] - x_mean) * (Y[i] - y_mean)
+        denominator += (X[i] - x_mean) ** 2
+
+    b1 = numerator / denominator
+    b0 = y_mean - (b1 * x_mean)
+    print(b1, b0)
+    plot(X, Y, b0, b1)
 
 if __name__ == '__main__':
     main()
