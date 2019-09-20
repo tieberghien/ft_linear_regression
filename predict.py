39e11cc7711b118bc46ced648b6a8098fbf30de0
Estelle TIEBERGHIEN <etieberg@e1r2p16.42.fr>
2019-09-20 15:24:10 +0200

    Linear regression done

---
+++ predict.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as py
+import matplotlib.pyplot as plt
+
+def main():
+
+if __name__ = '__main__'
+    main()
--- train.py
+++ train.py
@@ -3,18 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-def plotLine(theta0, theta1, X, y):
-    max_x = np.max(X) + 100
-    min_x = np.min(X) - 100
-
-    xplot = np.linspace(min_x, max_x, 1000)
-    yplot = theta0 + theta1 * xplot
-
-    plt.plot(xplot, yplot, color='#ff0000', label='Regression Line')
-    plt.scatter(X,y)
-    plt.axis([-10, 10, 0, 200])
-    plt.show()
-
 def predictPrice(x,theta):
     return np.dot(x,theta)
 
@@ -22,13 +10,13 @@ def calculateCost(x,theta,Y):
     prediction = predictPrice(x,theta)
     return ((prediction - Y)**2).mean()/2
 
-def plot(X, Y, b0, b1):
+def plotLine(X, Y, theta0, theta1):
 #plotting values 
     x_max = np.max(X) + 100
     x_min = np.min(X) - 100
     #calculating line values of x and y
     x = np.linspace(x_min, x_max, 1000)
-    y = b0 + b1 * x
+    y = theta0 + theta1 * x
     #plotting line 
     plt.plot(x, y, color='#00ff00', label='Linear Regression')
     #plot the data point
@@ -55,9 +43,9 @@ def main():
         numerator += (X[i] - x_mean) * (Y[i] - y_mean)
         denominator += (X[i] - x_mean) ** 2
 
-    b1 = numerator / denominator
-    b0 = y_mean - (b1 * x_mean)
-    print(b1, b0)
+    theta1 = numerator / denominator
+    theta0 = y_mean - (b1 * x_mean)
+    print(theta1, theta0)
     plot(X, Y, b0, b1)
 
 if __name__ == '__main__':
