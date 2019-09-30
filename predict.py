7b6d973e233e472c0c7582f0595b016dc43003d9
admin <admin@iMac-de-admin-8.local>
2019-09-30 12:56:29 +0200

    error handling

--- predict.py
+++ predict.py
@@ -1,8 +1,28 @@
+import sys
 import pandas as pd
 import numpy as py
 import matplotlib.pyplot as plt
 
-def main():
+def estimatePrice(x,theta):
+    return np.dot(x,theta)
 
-if __name__ = '__main__'
+def learningRate(x,theta,Y):
+    prediction = estimatePrice(x,theta)
+    return ((prediction - Y)**2).mean()/2
+
+def main():
+    print("Please enter valid mileage (int):")
+    for line in sys.stdin:
+        line.rstrip()
+        try:
+            mileage = int(line)
+            if mileage > 0:
+                break
+            else:
+                print("Mileage can't be negative, try again:")
+        except ValueError:
+            print("Error, please enter valid mileage (int):")
+    print(mileage)
+
+if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -3,36 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-def predictPrice(x,theta):
-    return np.dot(x,theta)
-
-def calculateCost(x,theta,Y):
-    prediction = predictPrice(x,theta)
-    return ((prediction - Y)**2).mean()/2
-
-def plotLine(X, Y, theta0, theta1):
-#plotting values 
-    x_max = np.max(X) + 100
-    x_min = np.min(X) - 100
-    #calculating line values of x and y
-    x = np.linspace(x_min, x_max, 1000)
-    y = theta0 + theta1 * x
-    #plotting line 
-    plt.plot(x, y, color='#00ff00', label='Linear Regression')
-    #plot the data point
-    plt.scatter(X, Y, color='#ff0000', label='Data Point')
-    # x-axis label
-    plt.xlabel('Kilometers')
-    #y-axis label
-    plt.ylabel('Price')
-    plt.legend()
-    plt.show()
-
-def main():
-    data = pd.read_csv("data.csv")
-    X = data['km'].values
-    Y = data['price'].values
-
+def linearRegression(X, Y):
     x_mean = np.mean(X)
     y_mean = np.mean(Y)
 
@@ -44,9 +15,37 @@ def main():
         denominator += (X[i] - x_mean) ** 2
 
     theta1 = numerator / denominator
-    theta0 = y_mean - (b1 * x_mean)
+    theta0 = y_mean - (theta1 * x_mean)
     print(theta1, theta0)
-    plot(X, Y, b0, b1)
+    plotLine(X, Y, theta0, theta1)
+
+def plotLine(X, Y, theta0, theta1):
+    x_max = np.max(X) + 100
+    x_min = np.min(X) - 100
+
+    x = np.linspace(x_min, x_max, 1000)
+    y = theta0 + theta1 * x
+
+    plt.plot(x, y, color='#00ff00', label='Linear Regression')
+    plt.scatter(X, Y, color='#ff0000', label='Data Point')
+    plt.xlabel('Kilometers')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
+
+def main():
+    try:
+        data = pd.read_csv("data.csv")
+    except FileNotFoundError as e:
+        print(e)
+        exit()
+    except pd.errors.EmptyDataError as e:
+        print(e)
+        exit()
+    X = data['km'].values
+    Y = data['price'].values
+
+    linearRegression(X, Y)
 
 if __name__ == '__main__':
     main()
