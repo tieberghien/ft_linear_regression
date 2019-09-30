3e36891a43f08dffc55904194baa7cf7edf5ae74
admin <admin@iMac-de-admin-8.local>
2019-09-30 16:28:28 +0200

    test

--- predict.py
+++ predict.py
@@ -4,10 +4,13 @@ import numpy as py
 import matplotlib.pyplot as plt
 import train
 
-def estimatePrice(mileage,theta0,theta1):
+theta0 = 0.0
+theta1 = 0.0
+
+def estimatePrice(mileage):
     return (float(theta0) + (float(theta1) * mileage))
 
-def predict(theta0,theta1):
+def predict():
     print("Please enter valid mileage (int):")
     for line in sys.stdin:
         line.rstrip()
@@ -19,17 +22,17 @@ def predict(theta0,theta1):
                 print("Mileage can't be negative, try again:")
         except ValueError:
             print("Error, please enter valid mileage (int):")
-    print(estimatePrice(mileage, theta0, theta1))
+    print(estimatePrice(mileage))
 
 if __name__ == '__main__':
     try:
         data = pd.read_csv("theta.csv")
     except FileNotFoundError as e:
-        predict(0, 0)
+        predict()
         exit()
     except pd.errors.EmptyDataError as e:
         print(e)
         exit()
     theta0 = data['0'].values
     theta1 = data['1'].values   
-    predict(theta0, theta1)
+    predict()
